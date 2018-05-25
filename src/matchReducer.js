b07d4c32c6b089b6bac8f2fec4a729c52a078d68
Elcoosp <elcoosp@gmail.com>
2018-05-25 17:04:26 +0200

    feat(matchReducer): Return a redux reducer ready to handle actions, initialState can be provided in

--- src/matchReducer.js
+++ src/matchReducer.js
@@ -1,2 +1,19 @@
-const matchReducer = (state, action) => {}
+const matchReducer = (actionTypeReducerTuples, options = {}) => (
+  state = options.initialState,
+  action
+) => {
+  for (const actionType in actionTypeReducerTuples) {
+    if (actionType === action.type) {
+      return Object.assign(
+        {},
+        state,
+        actionTypeReducerTuples[actionType](
+          state,
+          options.passOnlyPayload ? action.payload : action
+        )
+      )
+    }
+  }
+  return state
+}
 module.exports = matchReducer
